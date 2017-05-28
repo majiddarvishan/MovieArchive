4f0f9a70678dfb9342c426be7a3c53d0bb6ea585
Majid Darvishan <majid.darvishan@peykasa.ir>
2017-05-28 19:28:18 +0430

    Add delete function

--- WebApplication1/Controllers/MovieController.cs
+++ WebApplication1/Controllers/MovieController.cs
@@ -7,13 +7,14 @@ using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
+    [Authorize]
     public class MovieController : BaseController
     {
         // GET: Movie
         public ActionResult Index()
         {
             ViewBag.Movies = DbContext.Movies.ToList();
-            
+
             return View();
         }
         [HttpGet]
@@ -22,6 +23,7 @@ namespace WebApplication1.Controllers
             return View();
         }
         [HttpPost]
+        [ValidateAntiForgeryToken]        
         public ActionResult AddMovie(Movie model)
         {
             if (!model.IsValid())
@@ -32,7 +34,30 @@ namespace WebApplication1.Controllers
             DbContext.SaveChanges();
             return RedirectToAction("Index");
         }
+        [HttpGet]
+        public ActionResult DelMovie()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public JsonResult DelMovie(Movie model)
+        {
+            /*if (!model.IsValid())
+            {
+                return RedirectToAction("DelMovie");
+            }*/
+            try
+            {
+                DbContext.Movies.Attach(model);                
+                DbContext.Entry(model).State = System.Data.Entity.EntityState.Deleted;
+                DbContext.SaveChanges();
+            }
+            catch {
+                return Json(new { status = "fail", message = "Movie not found" });
+            }
+            return Json(new { status = "success" });
+        }
         
-
     }
 }
